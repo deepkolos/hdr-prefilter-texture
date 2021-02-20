3a34cbd488c696773dcd392b87375bcf37bf46ab
deepkolos <ouyanghaozhu@oppentech.com>
2021-02-20 14:51:40 +0800

    feat: use pngjs to encode png & show three version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-async-promise-executor */
 import './style';
 import flip from './flip';
 import { downloadBlob, blobToBase64 } from './utils.js';
@@ -18,6 +19,7 @@ import {
 } from 'three-platformize';
 import { BrowserPlatform } from 'three-platformize/src/BrowserPlatform';
 import { toEnvMap } from 'three-platformize/tools/toEnvMap';
+import { PNG } from 'pngjs/browser';
 
 PLATFORM.set(new BrowserPlatform());
 
@@ -35,7 +37,7 @@ const githubIcon = (
     <path
       fill-rule="evenodd"
       d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-    ></path>
+    />
   </svg>
 );
 
@@ -70,8 +72,9 @@ export default function App() {
   }, []);
 
   async function convert(file) {
-    const { renderer, rgbeLoader, pmremGenerator, ctx } = depsRef.current;
-    const canvas = canvas2DRef.current;
+    const { renderer, rgbeLoader, pmremGenerator } = depsRef.current;
+    // const canvas = canvas2DRef.current;
+    // const ctx = depsRef.current.ctx
 
     const base64 = await blobToBase64(file);
     const hdr = await new Promise(r => rgbeLoader.load(base64, r));
@@ -87,28 +90,43 @@ export default function App() {
     );
     flip(buffer, target.width, target.height, 4);
 
-    const imgData = new ImageData(
-      new Uint8ClampedArray(buffer),
-      target.width,
-      target.height,
-    );
-
-    canvas.width = target.width;
-    canvas.height = target.height;
-    ctx.putImageData(imgData, 0, 0);
-
     return new Promise(resolve => {
-      canvas.toBlob(
-        blob =>
-          resolve({
-            name: file.name.replace(/\.hdr$/i, '.png'),
-            blob,
-            file,
-            target,
-          }),
-        'image/png',
-        0.1,
-      );
+      const png = new PNG({
+        width: target.width,
+        height: target.height,
+        filterType: 0,
+      });
+      png.data = buffer;
+      const pngBuffer = PNG.sync.write(png);
+      const blob = new Blob([pngBuffer.buffer], { type: 'image/png' });
+      const pngFile = new File([blob], file.name.replace(/\.hdr$/i, '.png'));
+      resolve({
+        name: file.name.replace(/\.hdr$/i, '.png'),
+        blob,
+        file: pngFile,
+        target,
+      });
+
+      // const imgData = new ImageData(
+      //   new Uint8ClampedArray(buffer),
+      //   target.width,
+      //   target.height,
+      // );
+
+      // canvas.width = target.width;
+      // canvas.height = target.height;
+      // ctx.putImageData(imgData, 0, 0);
+      // canvas.toBlob(
+      //   blob =>
+      //     resolve({
+      //       name: file.name.replace(/\.hdr$/i, '.png'),
+      //       blob,
+      //       file,
+      //       target,
+      //     }),
+      //   'image/png',
+      //   0.1,
+      // );
     });
   }
 
@@ -175,6 +193,7 @@ export default function App() {
           href="https://github.com/deepkolos/hdr-prefilter-texture"
         >
           {githubIcon}HDRPrefilterTexture
+          <span className="version">r{REVISION}</span>
         </a>
         <div className="desc">
           导出PMREMGenerator生成的纹理，优化加载路径避免小程序部分机型BUG
@@ -199,21 +218,21 @@ export default function App() {
         <div>Exported</div>
       </div>
       <div className="diff-can">
-        <svg viewBox="0 0 2 1" style="width: 100%;"></svg>
+        <svg viewBox="0 0 2 1" style="width: 100%;" />
         <div className="diff">
           <canvas className="canvas-hdr" ref={glExportRef} />
         </div>
       </div>
 
       <div className="size-table">
-        <div className="size-table-item"></div>
+        <div className="size-table-item" />
         <div className="size-table-row">
           <div>文件名</div>
           <div>HDR</div>
           <div>PNG</div>
           <div>Saved</div>
         </div>
-        {sizeTable.map(({ name, original, exported }, i) => (
+        {sizeTable.map(({ name, original, exported }) => (
           <div className="size-table-row">
             <div>{name}</div>
             <div>{(original / 1024).toFixed(3)}kb</div>
